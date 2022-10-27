4c08ee6445245588f1d423ef805a14f5df9118ab
tajin002 <tajinshawon002@gmail.com>
2022-10-27 10:37:34 +0600

    blog  section added with question ans

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -1,75 +1,198 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            <section className="dark:bg-gray-800 dark:text-gray-100">
-                <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-                    <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
-                        <img src="https://source.unsplash.com/random/480x360" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
-                        <div className="p-6 space-y-2 lg:col-span-5">
-                            <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
-                            <span className="text-xs dark:text-gray-400">February 19, 2021</span>
-                            <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
-                        </div>
-                    </a>
-                    <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?1" />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-400">January 21, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
-                        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?2" />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-400">January 22, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
-                        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?3" />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-400">January 23, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
-                        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?4" />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-400">January 24, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
-                        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?5" />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-400">January 25, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
-                        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?6" />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-400">January 26, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="flex justify-center">
-                        <button type="button" className="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-900 dark:text-gray-400">Load more posts...</button>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div>
+      <div className="bg-gray-300 text-black">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+          <div className="flex items-center justify-between">
+            <span className="text-sm dark:text-gray-400">Jun 1, 2020</span>
+            <p className="px-2 py-1 font-bold rounded dark:bg-teal-400 dark:text-gray-900">
+              Node.js (Cors)
+            </p>
+          </div>
+          <div className="mt-3">
+            <p className="text-2xl font-bold hover:underline">What is Cors ?</p>
+            <p className="mt-2">
+              Cross-origin resource sharing (CORS) is a mechanism that allows
+              restricted resources on a web page to be requested from another
+              domain outside the domain from which the first resource was
+              served.A web page may freely embed cross-origin images,
+              stylesheets, scripts, iframes, and videos.Certain "cross-domain"
+              requests, notably Ajax requests, are forbidden by default by the
+              same-origin security policy. CORS defines a way in which a browser
+              and server can interact to determine whether it is safe to allow
+              the cross-origin request.It allows for more freedom and
+              functionality than purely same-origin requests, but is more secure
+              than simply allowing all cross-origin requests. The specification
+              for CORS is included as part of the WHATWG's Fetch Living
+              Standard.This specification describes how CORS is currently
+              implemented in browsers.An earlier specification was published as
+              a W3C Recommendation.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <div>
+              <p className="flex items-center">
+                <img
+                  src="https://source.unsplash.com/50x50/?portrait"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 mx-4 rounded-full dark:bg-gray-500"
+                />
+                <span className="hover:underline dark:text-gray-400">
+                  Tajin Shawon
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="bg-gray-300 text-black">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+          <div className="flex items-center justify-between">
+            <span className="text-sm dark:text-gray-400">Jun 1, 2020</span>
+            <p className="px-2 py-1 font-bold rounded dark:bg-teal-400 dark:text-gray-900">
+              Firebase
+            </p>
+          </div>
+          <div className="mt-3">
+            <p className="text-2xl font-bold hover:underline">
+              {" "}
+              Why are you using firebase?
+            </p>
+            <p className="mt-2">
+              The Firebase Realtime Database lets you build rich, collaborative
+              applications by allowing secure access to the database directly
+              from client-side code. Data is persisted locally, and even while
+              offline, realtime events continue to fire, giving the end user a
+              responsive experience.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <div>
+              <p className="flex items-center">
+                <img
+                  src="https://source.unsplash.com/50x50/?portrait"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 mx-4 rounded-full dark:bg-gray-500"
+                />
+                <span className="hover:underline dark:text-gray-400">
+                  Tajin Shawon
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-300 text-black">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+          <div className="flex items-center justify-between">
+            <span className="text-sm dark:text-gray-400">Jun 1, 2020</span>
+            <p className="px-2 py-1 font-bold rounded dark:bg-teal-400 dark:text-gray-900">
+              Firebase
+            </p>
+          </div>
+          <div className="mt-3">
+            <p className="text-2xl font-bold hover:underline">
+              What other options do you have to implement authentication?
+            </p>
+            <p className="mt-2">
+              User authentication is a method that keeps unauthorized users from
+              accessing sensitive information. For example, User A only has
+              access to relevant information and cannot see the sensitive
+              information of User B. Cyber criminals can gain access to a system
+              and steal information when user authentication is not secure. The
+              data breaches companies like Adobe, Equifax, and Yahoo faced are
+              examples of what happens when organizations fail to secure their
+              user authentication. Hackers gained access to Yahoo user accounts
+              to steal contacts, calendars and private emails between 2012 and
+              2016. The Equifax data breach in 2017 exposed credit card data of
+              more than 147 million consumers. Without a secure authentication
+              process, any organization could be at risk.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <div>
+              <p className="flex items-center">
+                <img
+                  src="https://source.unsplash.com/50x50/?portrait"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 mx-4 rounded-full dark:bg-gray-500"
+                />
+                <span className="hover:underline dark:text-gray-400">
+                  Tajin Shawon
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-300 text-black">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+          <div className="flex items-center justify-between">
+            <span className="text-sm dark:text-gray-400">Jun 1, 2020</span>
+            <p className="px-2 py-1 font-bold rounded dark:bg-teal-400 dark:text-gray-900">
+              Node.js
+            </p>
+          </div>
+          <div className="mt-3">
+            <p className="text-2xl font-bold hover:underline">
+            What is Node? 
+            </p>
+            <p className="mt-2">
+            Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <div>
+              <p className="flex items-center">
+                <img
+                  src="https://source.unsplash.com/50x50/?portrait"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 mx-4 rounded-full dark:bg-gray-500"
+                />
+                <span className="hover:underline dark:text-gray-400">
+                  Tajin Shawon
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-300 text-black">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+          <div className="flex items-center justify-between">
+            <span className="text-sm dark:text-gray-400">Jun 1, 2020</span>
+            <p className="px-2 py-1 font-bold rounded dark:bg-teal-400 dark:text-gray-900">
+              Node.js
+            </p>
+          </div>
+          <div className="mt-3">
+            <p className="text-2xl font-bold hover:underline">
+            How does Node work?
+            </p>
+            <p className="mt-2">
+            It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <div>
+              <p className="flex items-center">
+                <img
+                  src="https://source.unsplash.com/50x50/?portrait"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 mx-4 rounded-full dark:bg-gray-500"
+                />
+                <span className="hover:underline dark:text-gray-400">
+                  Tajin Shawon
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
